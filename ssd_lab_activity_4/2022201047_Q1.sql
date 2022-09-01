5218024d6ac7196418d97b49436149fabd3c8f58
priyank <priyank@DESKTOP-IEBU6DB.localdomain>
2022-09-01 14:58:07 +0530

    Q1 Done

---
+++ ssd_lab_activity_4/2022201047_Q1.sql
@@ -0,0 +1,9 @@
+drop procedure if exists add_num;
+DELIMITER &&
+CREATE PROCEDURE add_num(IN x int,IN y int,INOUT sum int)
+BEGIN
+ set sum=x+y;
+ END &&
+ 
+ CALL add_num (12,5,@sum);
+ select @sum;
